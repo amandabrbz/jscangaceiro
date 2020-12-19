9da1e9abc59f2bf8f5162581a3ad82b8ef6b526b
Amanda Barboza <amanda.barboza@mirumagency.com>
2020-12-19 01:16:45 -0300

    transformando o obj imutavel

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,8 +1,8 @@
 class Negociacao {
   constructor(data, quantidade, valor) {
-    this._data = new Date(data.getTime());
-    this._quantidade = quantidade;
-    this._valor = valor;
+    Object.assign(this, {
+      _data = new Date(data.getTime()), _quantidade = quantidade, _valor = valor;
+    })
     Object.freeze(this);
   }
 
